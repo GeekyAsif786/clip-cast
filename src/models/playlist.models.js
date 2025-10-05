997160860c4f956d48804667374a0e6446c72e96
GeekyAsif786 <maniurrahaman1@gmail.com>
2025-10-05 11:17:27 +0530

    added playlist model

---
+++ src/models/playlist.models.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const playlistSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        index:true,
+    },
+    description:{
+        type:true,
+        required:false,
+    },
+    //array of videos
+    videos:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video",
+        }
+    ],
+    //owner of the playlist
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    }
+},{timestamps:true})
+
+export const Playlist = mongoose.model("Playlist",playlistSchema)
+
+
