e436cd00c52afa009ed4827adc140666222f1e73
GeekyAsif786 <techwise.asif.dev@gmail.com>
2025-10-26 00:23:42 +0530

    refactor(playlist): reorganize routes and ensure JWT middleware is applied to all endpoints

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,1 +1,33 @@
-router.patch("/:playlistId/visibility", verifyJWT, togglePlaylistVisibility);
+import { Router } from 'express';
+import {
+    addVideoToPlaylist,
+    createPlaylist,
+    deletePlaylist,
+    getPlaylistById,
+    getUserPlaylists,
+    removeVideoFromPlaylist,
+    togglePlaylistVisibility,
+    updatePlaylist,
+} from "../controllers/playlist.controllers.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/").post(createPlaylist)
+
+router
+    .route("/:playlistId")
+    .get(getPlaylistById)
+    .patch(updatePlaylist)
+    .delete(deletePlaylist);
+router.route("/:playlistId/visibility").patch(verifyJWT,togglePlaylistVisibility)
+router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
+router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+router.route("/user/:userId").get(getUserPlaylists);
+
+export default router
+
+
+
