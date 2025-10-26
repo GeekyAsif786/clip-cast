0c7929b814fdc4dc5633fb76862a8e5dbc1b1f3b
GeekyAsif786 <techwise.asif.dev@gmail.com>
2025-10-26 21:31:22 +0530

    feat(activityLog): add activity log model to track user actions on tweets, videos, and playlists

--- src/models/tweet.models.js
+++ src/models/tweet.models.js
@@ -17,7 +17,35 @@ const tweetSchema = new Schema({
     likeCount:{
         type:Number,
         default:0
-    }
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+    deletedAt: {
+        type: Date,
+        default: null,
+    },
+    editedAt: {
+        type: Date,
+        default: null,
+    },
 },{timestamps:true})
 
+//Pre-query middleware (soft delete filter)
+tweetSchema.pre(/^find/, function(next) {
+    if (!this.getFilter().includeDeleted) {
+        this.where({ isDeleted: false });
+    }
+    next();
+});
+
+//pre-aggregate hook (if you use aggregations)
+tweetSchema.pre("aggregate", function(next) {
+    const matchStage = { $match: { isDeleted: false } };
+    this.pipeline().unshift(matchStage);
+    next();
+});
+
+tweetSchema.index({ owner: 1, isDeleted: 1, createdAt: -1 });
 export const Tweet = mongoose.model("Tweet",tweetSchema)
