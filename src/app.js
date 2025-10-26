c9cb889ec90e8986f22b119531a9685f3ce71c30
GeekyAsif786 <techwise.asif.dev@gmail.com>
2025-10-26 21:31:46 +0530

    feat(tweet): add tweet router for tweet-related API endpoints

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ import commentRouter from './routes/comment.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import healthcheckRouter from './routes/healthcheck.routes.js'
+import tweetRouter from './routes/tweet.routes.js'
 //routes declaration
 //*since we have separated routes and controllers folders,so we need to use middleware to use routes
 app.use("/api/v1/healthcheck", healthcheckRouter)
@@ -35,4 +36,5 @@ app.use("/api/v1/likes",likeRouter) //? http://localhost:<port>/api/v1/likes/<ro
 app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/playlist",playlistRouter)
+app.use("/api/v1/tweets", tweetRouter)
 export { app } 
