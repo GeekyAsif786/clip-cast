005623ed6930fd7878a0bc5306eaac16d26900b9
GeekyAsif786 <techwise.asif.dev@gmail.com>
2025-10-27 17:54:39 +0530

    feat(api): update ApiResponse success property to reflect error status correctly

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ import subscriptionRouter from './routes/subscription.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import healthcheckRouter from './routes/healthcheck.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
+import dashboardRouter from './routes/dashboard.routes.js'
 //routes declaration
 //*since we have separated routes and controllers folders,so we need to use middleware to use routes
 app.use("/api/v1/healthcheck", healthcheckRouter)
@@ -37,4 +38,5 @@ app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/playlist",playlistRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 export { app } 
