2ef4f33b87bd7a5715874196224018b3ae10a756
GeekyAsif786 <techwise.asif.dev@gmail.com>
2025-11-01 18:22:07 +0530

    feat(rateLimit): update auth rate limit keys to use ipKeyGenerator for login and registration

--- src/config/rateLimits.js
+++ src/config/rateLimits.js
@@ -6,8 +6,8 @@ export const RATE_LIMITS_VIDEO = {
 };
 
 export const RATE_LIMITS_AUTH = {
-    loginUser: {window: 15 * 60 * 1000, max: 10, key: (req) => req.ip, message: "Too many login attempts, try after 15 minutes", },
-    registerUser: {window: 15 * 60 * 1000, max: 5, key: (req) => req.ip, message: "Too many attempts, kindly slow down"},
+    loginUser: {window: 15 * 60 * 1000, max: 10, key: (req) => ipKeyGenerator(req), message: "Too many login attempts, try after 15 minutes", },
+    registerUser: {window: 15 * 60 * 1000, max: 5, key: (req) => ipKeyGenerator(req), message: "Too many attempts, kindly slow down"},
     changeCurrentPassword: {window: 60 * 60 * 1000, max: 3, key: (req) => req.user?._id?.toString(), message: "Too many attempts, try after 1 hour"},
     updateAccountDetails: {window: 10 * 60 * 1000, max: 10, key: (req) => req.user?._id?.toString(), message: "Too many update attempts, try again after 10 minutes"},
     updateUserAvatar: {window: 60 * 60 * 1000, max: 3, key: (req) => req.user?._id?.toString(), message: "Too many update attempts, try again after 10 minutes"},
