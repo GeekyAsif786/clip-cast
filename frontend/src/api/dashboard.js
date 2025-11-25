f4607b76e9419818ef2b22ed134aeed1d45e0d04
Asifur Rahaman <asif@Asifurs-MacBook-Air.local>
2025-11-26 04:44:59 +0530

    Remove frontend folder from main branch

--- frontend/src/api/dashboard.js
+++
@@ -1,4 +0,0 @@
-import api from './axios';
-
-export const getChannelStats = () => api.get('/dashboard/stats');
-export const getChannelVideos = () => api.get('/dashboard/videos');
--- frontend/src/api/subscription.js
+++
@@ -1,5 +0,0 @@
-import api from './axios';
-
-export const toggleSubscription = (channelId) => api.post(`/subscriptions/c/${channelId}`);
-export const getUserChannelSubscribers = (channelId) => api.get(`/subscriptions/c/${channelId}`);
-export const getSubscribedChannels = (subscriberId) => api.get(`/subscriptions/u/${subscriberId}`);
